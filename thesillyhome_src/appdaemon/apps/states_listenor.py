03b3d9d217366e416537dc34f0093fcb282c443c
unknown <lcmchristopher@gmail.com>
2022-09-05 08:34:50 +0800

    Adding exploratory data notebooks.

--- thesillyhome_src/appdaemon/apps/states_listenor.py
+++ thesillyhome_src/appdaemon/apps/states_listenor.py
@@ -13,49 +13,26 @@ import uuid
 import thesillyhome.model_creator.read_config_json as tsh_config
 
 
-
 class StatesListenor(hass.Hass):
     def initialize(self):
         self.extdb = self.connect_external_db()
         self.handle = self.listen_state(self.state_handler)
-        self.loop = self.periodic_log_state_daemon()
         self.user = hex(uuid.getnode())
-
 
         self.log("Hello from TheSillyHome")
         self.log("TheSillyHome state listenor fully initialized!")
 
-    def periodic_log_state_daemon(self):
-        thread = threading.Thread(target=self.periodic_log_state, daemon=True)
-        thread.start()
-
-    def periodic_log_state(self):
-        while True:
-            data_export = {
-                "snapshot_time": str(datetime.now()),
-                "get_state_json": json.dumps(self.get_state()),
-                "type": "peri",
-                "user_id": self.user,
-            }
-            data = pd.DataFrame(data_export, index=[0])
-            data.to_sql(
-                "states_snapshots", con=self.extdb, index=False, if_exists="append"
-            )
-            time.sleep(5.0 - time.time() % 5.0)
-
     def state_handler(self, entity, attribute, old, new, kwargs):
-
-        if entity in tsh_config.devices:
-            data_export_event = {
-                "snapshot_time": str(datetime.now()),
-                "get_state_json": json.dumps(self.get_state()),
-                "type": "chng",
-                "user_id": self.user,
-            }
-            data_event = pd.DataFrame(data_export_event, index=[0])
-            data_event.to_sql(
-                "states_snapshots", con=self.extdb, index=False, if_exists="append"
-            )
+        data_export_event = {
+            "snapshot_time": str(datetime.now()),
+            "get_state_json": json.dumps(self.get_state()),
+            "type": "chng",
+            "user_id": self.user,
+        }
+        data_event = pd.DataFrame(data_export_event, index=[0])
+        data_event.to_sql(
+            "states_snapshots", con=self.extdb, index=False, if_exists="append"
+        )
 
     def connect_external_db(self):
         host = tsh_config.extdb_host

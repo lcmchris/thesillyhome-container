8205d2d334d63afad6f93ebb5e88fb21cd54b589
Simon <52816978+Lightningpicture@users.noreply.github.com>
2023-05-03 07:59:14 +0200

    Update main.py

--- thesillyhome_src/thesillyhome/src/thesillyhome/model_creator/main.py
+++ thesillyhome_src/thesillyhome/src/thesillyhome/model_creator/main.py
@@ -4,7 +4,7 @@ from thesillyhome.model_creator.read_config_json import replace_yaml
 from thesillyhome.model_creator.read_config_json import run_cron
 from thesillyhome.model_creator.parse_data import parse_data_from_db
 from thesillyhome.model_creator.learning_model import train_all_actuator_models
-#from thesillyhome.model_creator.logger import add_logger
+from thesillyhome.model_creator.logger import add_logger
 from thesillyhome.model_creator.config_checker import base_config_checks
 
 

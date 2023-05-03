44595b549e0ceebabdd4b7a4915fe2ca0857ef05
Simon <52816978+Lightningpicture@users.noreply.github.com>
2023-05-03 11:37:23 +0200

    Update home.py
    Fix Update timestamp
    Fix Changes timestamp
    Limit to 1000

--- thesillyhome_src/thesillyhome/src/thesillyhome/model_creator/home.py
+++ thesillyhome_src/thesillyhome/src/thesillyhome/model_creator/home.py
@@ -60,13 +60,13 @@ class homedb:
                     states.state_id AS state_id  ,\
                     states_meta.entity_id AS entity_id  ,\
                     states.state AS state  ,\
-                    states.last_changed AS last_changed  ,\
-                    states.last_updated AS last_updated  ,\
+                    states.last_changed_ts AS last_changed  ,\
+                    states.last_updated AS_ts last_updated  ,\
                     states.old_state_id AS old_state_id  \
                 from states \
                 JOIN states_meta ON states.metadata_id = states_meta.metadata_id\
                 WHERE states_meta.entity_id in ({str(tsh_config.devices)[1:-1]})\
-                ORDER BY last_updated DESC LIMIT 100000;"
+                ORDER BY last_updated DESC LIMIT 1000;"
         with self.mydb.connect() as con:
             con = con.execution_options(stream_results=True)
             list_df = [

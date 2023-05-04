9f8bcc5438b1af61249ebae5dd587a675e1afcfc
Simon <52816978+Lightningpicture@users.noreply.github.com>
2023-05-04 08:20:58 +0200

    Update home.py

--- thesillyhome_src/thesillyhome/src/thesillyhome/model_creator/home.py
+++ thesillyhome_src/thesillyhome/src/thesillyhome/model_creator/home.py
@@ -66,7 +66,7 @@ class homedb:
                 from states \
                 JOIN states_meta ON states.metadata_id = states_meta.metadata_id\
                 WHERE states_meta.entity_id in ({str(tsh_config.devices)[1:-1]})\
-                ORDER BY states.last_changed_ts DESC LIMIT 10000;"
+                ORDER BY states.last_changed_ts DESC LIMIT 2;"
         with self.mydb.connect() as con:
             con = con.execution_options(stream_results=True)
             list_df = [

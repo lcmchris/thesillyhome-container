188c0fc24784103a366a6d1d69c3154fd025de7b
Simon <52816978+Lightningpicture@users.noreply.github.com>
2024-09-07 00:07:49 +0200

    Update learning_model.py
    Change Learning

--- thesillyhome_src/thesillyhome/src/thesillyhome/model_creator/learning_model.py
+++ thesillyhome_src/thesillyhome/src/thesillyhome/model_creator/learning_model.py
@@ -2,15 +2,19 @@
 from cmath import exp
 import os
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import precision_recall_curve
+
 import sys
 import numpy as np
+
 from sklearn.metrics import accuracy_score, precision_score, recall_score, auc
+
 import pickle
 import logging
 import matplotlib.pyplot as plt

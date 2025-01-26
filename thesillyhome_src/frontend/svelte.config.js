a7b58e812c3bee7719e2180270196deb0d81b5fb
Simon <52816978+Lightningpicture@users.noreply.github.com>
2025-01-26 21:46:21 +0100

    Add files via upload

---
+++ thesillyhome_src/frontend/svelte.config.js
@@ -0,0 +1,24 @@
+import adapter from '@sveltejs/adapter-node';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    preprocess: preprocess(),
+
+    kit: {
+        adapter: adapter(),
+        paths: {
+            base: process.env.INGRESS_ENTRY || '', // Dynamische Basis-URL für Ingress
+        },
+        vite: {
+            server: {
+                fs: {
+                    // Zugriff auf statische Dateien erlauben
+                    allow: ['static'],
+                },
+            },
+        },
+    },
+};
+
+export default config;

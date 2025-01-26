e6346f1ba5cb9acc86ddfed3fd8e899d639ef616
Simon <52816978+Lightningpicture@users.noreply.github.com>
2025-01-26 21:43:38 +0100

    Delete thesillyhome_src/frontend directory

--- thesillyhome_src/frontend/src/routes/api/GetImage.ts
+++
@@ -1,6 +0,0 @@
-import { readFileSync } from 'fs'
-
-export async function get(request) {
-    const data = readFileSync(request.url.searchParams.get('path'), 'base64')
-    return { body: { message: data } };
-}

e549b191b4a995873686ae842650e4c92f1f1759
Simon <52816978+Lightningpicture@users.noreply.github.com>
2025-01-29 11:50:17 +0100

    Update parse_data.py

--- thesillyhome_src/thesillyhome/src/thesillyhome/model_creator/parse_data.py
+++ thesillyhome_src/thesillyhome/src/thesillyhome/model_creator/parse_data.py
@@ -4,13 +4,16 @@ import numpy as np
 import logging
 
 # Local application imports
-from thesillyhome.model_creator.home import HomeDB
+from thesillyhome.model_creator.home import homedb
 from thesillyhome.model_creator.logger import add_logger
 from thesillyhome.model_creator.config_checker import check_device_ids
 import thesillyhome.model_creator.read_config_json as tsh_config
 
 
 def get_current_states(df_output: pd.DataFrame) -> pd.DataFrame:
+    """
+    Returns pivoted frame of each state id desc
+    """
     df_pivot = (
         df_output.reset_index()
         .pivot(index=["state_id"], columns=["entity_id"], values=["state"])
@@ -22,12 +25,15 @@ def get_current_states(df_output: pd.DataFrame) -> pd.DataFrame:
 
 
 def add_duplicate(df_output: pd.DataFrame) -> pd.DataFrame:
+
     for entity in df_output["entity_id"].unique():
         df_filtered = df_output[df_output["entity_id"] == entity]
         df_shift = df_filtered.shift(-1)
+
         df_output.loc[df_output["entity_id"] == entity, "duplicate"] = np.where(
             df_filtered["state"] == df_shift["state"], 1, 3
         )
+
     return df_output
 
 
@@ -38,28 +44,40 @@ def one_hot_encoder(df: pd.DataFrame, column: str) -> pd.DataFrame:
     return df
 
 
-def convert_unavailable(df: pd.DataFrame) -> pd.DataFrame:
+def convert_unavailabe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    The fact is if a device such as a light bulb is powered off,
+    After a timeframe (availability_timeout), it is set to the 'unavailable' status
+
+    ['entity_id','states']
+    """
     conditions = [
         (df["entity_id"].isin(tsh_config.float_sensors))
         & (df["state"].isin([np.NaN, "unknown", "", "unavailable", None])),
         (~df["entity_id"].isin(tsh_config.float_sensors))
         & (df["state"].isin([np.NaN, "unknown", "", "unavailable", None])),
     ]
+
     choices = [0, "off"]
     return np.select(conditions, choices, default=df["state"])
 
 
 def parse_data_from_db():
-    logging.info("Reading from HomeDB...")
-    db = HomeDB()  # Instantiate the database connection
-    df_all = db.get_data()
+    """
+    Our data base currently stores by events.
+    To create a valid ML classification case, we will parse all last
+    sensor states for each actuator event and append it to the dataframe.
+    """
+
+    logging.info("Reading from homedb...")
+    df_all = homedb().get_data()
     df_all = df_all[["entity_id", "state", "last_updated"]]
 
     check_device_ids(df_all["entity_id"].unique())
     logging.info(tsh_config.actuators)
     logging.info(tsh_config.sensors)
 
-    df_all["state"] = convert_unavailable(df_all)
+    df_all["state"] = convert_unavailabe(df_all)
     assert ~df_all["state"].isnull().values.any(), df_all[df_all["state"].isnull()]
 
     df_all = df_all[df_all["entity_id"].isin(tsh_config.devices)]
@@ -74,6 +92,10 @@ def parse_data_from_db():
 
     df_output = add_duplicate(df_output)
 
+    """
+    Code to add one hot encoding for date time.
+    This will help give features for time of day and day of the week.
+    """
     df_output["last_updated"] = pd.to_datetime(df_output["last_updated"])
     df_output["hour"] = df_output["last_updated"].dt.hour
     df_output["weekday"] = df_output["last_updated"].dt.date.apply(
@@ -81,14 +103,23 @@ def parse_data_from_db():
     )
     df_output = df_output.drop(columns=["last_updated"])
 
+    """
+    feature list extraction
+    """
     output_list = tsh_config.output_list.copy()
     output_list.append("duplicate")
     feature_list = sorted(list(set(df_output.columns) - set(output_list)))
 
+    """
+    Hot encoding for all columns bar float_sensors which has int format
+    """
     for feature in feature_list:
         if feature not in tsh_config.float_sensors:
             df_output = one_hot_encoder(df_output, feature)
 
+    """
+    Output and checks
+    """
     df_output["state"] = np.where(df_output["state"] == "on", 1, 0)
 
     assert df_output[df_output["entity_id"] == ""].empty
